b2cf63fc065ca396bb8200d7c649215c9d69f736
klawgolfer7 <kevinrlawler@gmail.com>
2017-04-09 22:00:34 -0400

    adding final files

--- models/burger.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(sequelize, DataTypes){
-	var Burger = sequelize.define("Burger", {
-		burger_name: DataTypes.STRING, 
-		devoured: DataTypes.BOOLEAN,
-	});
-	return Burger;
-}
--- server.js
+++ server.js
@@ -1,42 +1,74 @@
-//DEPENDENCIES
-//=================================================================================
-//REQUIRE:
-//express
-var express = require("express");
-//body-parser
-var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 3000;
+// @author: Thomas Thompson
+// @github: tomtom28
+// @comment: Homework 18 - Web Scraper with Express, Handlebars, MongoDB and Cheerio
 
 
-var db = require("./models");
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({type:"application/vnd.api+json" }));
-
-app.use(methodOverride('_method'))
+// Node Dependencies
+var express = require('express');
 var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({defaultLayout:"main"}));
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var logger = require('morgan'); // for debugging
+var request = require('request'); // for web-scraping
+var cheerio = require('cheerio'); // for web-scraping
+
+
+// Initialize Express for debugging & body parsing
+var app = express();
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+// Serve Static Content
+app.use(express.static(process.cwd() + '/public'));
+
+// Express-Handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-// Static directory
-app.use(express.static("./public"));
 
-// Routes =============================================================
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+// Database Configuration with Mongoose
+// ---------------------------------------------------------------------------------------------------------------
+// Connect to localhost if not a production environment
+if(process.env.NODE_ENV == 'production'){
+  mongoose.connect('mongodb://heroku_60zpcwg0:ubn0n27pi2856flqoedo9glvh8@ds119578.mlab.com:19578/heroku_60zpcwg0');
+}
+else{
+  mongoose.connect('mongodb://localhost/news-scraper');
+  // YOU CAN IGNORE THE CONNECTION URL BELOW (LINE 41) THAT WAS JUST FOR DELETING STUFF ON A RE-DEPLOYMENT
+  //mongoose.connect('mongodb://heroku_60zpcwg0:ubn0n27pi2856flqoedo9glvh8@ds119578.mlab.com:19578/heroku_60zpcwg0');
+}
+var db = mongoose.connection;
 
-// Syncing our sequelize models and then starting our express app
-db.sequelize.sync({}).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+// Show any Mongoose errors
+db.on('error', function(err) {
+  console.log('Mongoose Error: ', err);
 });
 
+// Once logged in to the db through mongoose, log a success message
+db.once('open', function() {
+  console.log('Mongoose connection successful.');
+});
+
+// Import the Comment and Article models
+var Comment = require('./models/Comment.js');
+var Article = require('./models/Article.js');
+// ---------------------------------------------------------------------------------------------------------------
+
+// DROP DATABASE (FOR MY PERSONAL REFERENCE ONLY - YOU CAN IGNORE)
+// Article.remove({}, function(err) { 
+//    console.log('collection removed') 
+// });
+
+// Import Routes/Controller
+var router = require('./controllers/controller.js');
+app.use('/', router);
 
 
+// Launch App
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log('Running on port: ' + port);
+});
